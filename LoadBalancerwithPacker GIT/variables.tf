007e6c9154dacb04b34e1c07c30f1c23465e39ea
pikukesar <47484727+pikukesar@users.noreply.github.com>
2021-10-19 15:29:12 -0400

    Update variables.tf

--- LoadBalancerwithPacker GIT/variables.tf
+++ LoadBalancerwithPacker GIT/variables.tf
@@ -9,4 +9,34 @@ variable "location" {
   type        = string
   default     = "East US"
 }
+variable "resource_group" {
+  description = "Name of the resource group, including the -rg"
+  default     = "myResourceGroup"
+  type        = string
+}
 
+variable "username" {
+  description = "Enter username to associate with the machine"
+} 
+
+variable "password" {
+  description = "Enter password to use to access the machine"
+}
+
+variable "packer_resource_group" {
+  description = "Resource group of the Packer image"
+  default     =  "myResourceGroup"
+  type        = string
+}
+
+variable "packer_image_name" {
+  description = "Image name of the Packer image"
+  default     =  "myPackerImagePK"
+  type        = string
+}
+
+variable "num_of_vms" {
+  description = "Number of VM resources to be  created behnd the load balancer"
+  default     = 2
+  type        = number
+}
