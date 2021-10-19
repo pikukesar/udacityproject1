b427c440cd6946d235eff361bb1754e7edf83e21
pikukesar <47484727+pikukesar@users.noreply.github.com>
2021-10-19 15:28:35 -0400

    Update main.tf

--- LoadBalancerwithPacker GIT/main.tf
+++ LoadBalancerwithPacker GIT/main.tf
@@ -1,176 +1,120 @@
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  subscription_id = "XXXXX"
-  client_id       = "XXXXX"
-  client_secret   = "XXXXX"
-  tenant_id       = "XXXXX"
   features {}
 }
 
-# Locate the existing resource group
-data "azurerm_resource_group" "main" {
-  name = "myResourceGroup"
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group
+  location = var.location
+  tags     = var.tags
 }
 
-output "id" {
-  value = data.azurerm_resource_group.main.id
-}
-
-# Locate the existing custom image
-data "azurerm_image" "main" {
-  name                = "myPackerImagePK"
-  resource_group_name = "myResourceGroup"
-}
-
-output "image_id" {
-  value = "/subscriptions/522b5d75-2fbe-4fb5-b2d5-6c15a215feca/resourceGroups/RG-EASTUS-SPT-PLATFORM/providers/Microsoft.Compute/images/myPackerImagePK"
-}
-
-# Number of instance to be created
-locals {
-  instance_count = 2
-}
-
-#Create Virtual Network
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/24"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = var.tags
 }
 
-#Create internal Subnet inside Virtual Network
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_subnet" "main" {
+ name                 = "${var.prefix}-subnet"
+ resource_group_name  = azurerm_resource_group.main.name
+ virtual_network_name = azurerm_virtual_network.main.name
+ address_prefixes     = ["10.0.0.0/24"]
 }
 
-#Create Public IP address
-resource "azurerm_public_ip" "pip" {
-  name                = "${var.prefix}-pip"
-  resource_group_name = data.azurerm_resource_group.main.name
-  location            = data.azurerm_resource_group.main.location
-  allocation_method   = "Dynamic"
-}
-
-#Create network interface
 resource "azurerm_network_interface" "main" {
-  count               = local.instance_count
-  name                = "${var.prefix}-nic${count.index}"
-  resource_group_name = data.azurerm_resource_group.main.name
-  location            = data.azurerm_resource_group.main.location
+ count               = var.num_of_vms
+ name                = "${var.prefix}-${count.index}-nic"
+ location            = var.location
+ resource_group_name = azurerm_resource_group.main.name
 
-  ip_configuration {
-    name                          = "primary"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
+ ip_configuration {
+   name                          = "mainConfiguration"
+   subnet_id                     = azurerm_subnet.main.id
+   private_ip_address_allocation = "Dynamic"
+ }
+ tags = var.tags
 }
 
-# Create a availability set
-resource "azurerm_availability_set" "avset" {
-  name                         = "${var.prefix}avset"
-  location                     = data.azurerm_resource_group.main.location
-  resource_group_name          = data.azurerm_resource_group.main.name
-  platform_fault_domain_count  = 2
-  platform_update_domain_count = 2
-  managed                      = true
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-public-ip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  allocation_method   = "Static"
+  tags                = var.tags
 }
 
-# Create a Network Security Group with rules
-resource "azurerm_network_security_group" "webserver" {
-  name                = "tls_webserver"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
-  security_rule {
-    access                     = "Allow"
-    direction                  = "Inbound"
-    name                       = "tls"
-    priority                   = 100
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    source_address_prefix      = "*"
-    destination_port_range     = "443"
-    destination_address_prefix = azurerm_subnet.internal.address_prefix
-  }
+resource "azurerm_lb" "main" {
+ name                = "${var.prefix}-lb"
+ location            = azurerm_resource_group.main.location
+ resource_group_name = azurerm_resource_group.main.name
+
+ frontend_ip_configuration {
+   name                 = "publicIPAddress"
+   public_ip_address_id = azurerm_public_ip.main.id
+ }
+
+ tags = var.tags
 }
 
-# Create a Load Balancer
-resource "azurerm_lb" "example" {
-  name                = "${var.prefix}-lb"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
-
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.pip.id
-  }
+resource "azurerm_lb_backend_address_pool" "main" {
+ resource_group_name = azurerm_resource_group.main.name
+ loadbalancer_id     = azurerm_lb.main.id
+ name                = "BackEndAddressPool"
 }
 
-# Create a address Pool
-resource "azurerm_lb_backend_address_pool" "example" {
-  #resource_group_name = data.azurerm_resource_group.main.name
-  loadbalancer_id = azurerm_lb.example.id
-  name            = "BackEndAddressPool"
+
+resource "azurerm_availability_set" "availset" {
+ name                         = "availset"
+ location                     = azurerm_resource_group.main.location
+ resource_group_name          = azurerm_resource_group.main.name
+ platform_fault_domain_count  = 2
+ platform_update_domain_count = 2
+ managed                      = true
+tags = var.tags
 }
 
-resource "azurerm_lb_nat_rule" "example" {
-  resource_group_name            = data.azurerm_resource_group.main.name
-  loadbalancer_id                = azurerm_lb.example.id
-  name                           = "HTTPSAccess"
-  protocol                       = "Tcp"
-  frontend_port                  = 443
-  backend_port                   = 443
-  frontend_ip_configuration_name = azurerm_lb.example.frontend_ip_configuration[0].name
+data "azurerm_resource_group" "packer_rg" {
+  name = var.packer_resource_group
 }
 
-resource "azurerm_network_interface_backend_address_pool_association" "example" {
-  count                   = local.instance_count
-  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
-  ip_configuration_name   = "primary"
-  network_interface_id    = element(azurerm_network_interface.main.*.id, count.index)
+data "azurerm_image" "image" {
+  name                = var.packer_image_name
+  resource_group_name = data.azurerm_resource_group.packer_rg.name
 }
 
-resource "azurerm_virtual_machine" "MYVM" {
-  count                            = local.instance_count
-  name                             = "${var.prefix}-vm${count.index}"
-  resource_group_name              = data.azurerm_resource_group.main.name
-  location                         = data.azurerm_resource_group.main.location
-  vm_size                          = "Standard_DS12_v2"
-  delete_os_disk_on_termination    = true
-  delete_data_disks_on_termination = true
-  availability_set_id              = azurerm_availability_set.avset.id
-  network_interface_ids = [
-    azurerm_network_interface.main[count.index].id,
-  ]
 
-  storage_image_reference {
-    id = data.azurerm_image.main.id
+resource "azurerm_virtual_machine" "main" {
+  count                           = var.num_of_vms
+  name                            = "${var.prefix}${count.index}-vm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  vm_size                            = "Standard_B1s"
+  network_interface_ids = [element(azurerm_network_interface.main.*.id, count.index)]
 
-  }
-
-
-  storage_os_disk {
-    name              = "myVM2-OS"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+  storage_image_reference  {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 
   os_profile {
-    computer_name  = "APPVM"
-    admin_username = "devopsadmin"
-    admin_password = "Cssladmin#2019"
-  }
+   computer_name  =  "hostname${count.index}"
+   admin_username                  = "testadmin"
+   admin_password                  = "Password1234"
+ }
 
   os_profile_linux_config {
-    disable_password_authentication = false
+   disable_password_authentication = false
+ }
+
+  storage_os_disk  {
+   name              = "WSdisk${count.index}"
+   caching           = "ReadWrite"
+   create_option     = "FromImage"
+   managed_disk_type = "Standard_LRS"
   }
 
-
-  tags = {
-    environment = "Production"
-  }
 }
